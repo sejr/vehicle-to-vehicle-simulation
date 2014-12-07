9e8cffa357248a8d00f5570afe8a32897b50055e
Samuel Roth <sejr@me.com>
2014-12-07 16:33:16 -0500

    Added socket IO

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/demo', function(req, res){
+  res.sendFile(__dirname + '/demo.html');
+});
+
 app.get('/about', function(req, res){
 	res.send('<h1>About</h1>');
+});
+
+io.on('connection', function(socket){
+  console.log('A user has connected.');
 });
 
 http.listen(3000, function(){
