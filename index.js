aa5755b9138c9af88b1f6eb30dabc8319f8bd0fd
Samuel Roth <sejr@me.com>
2014-12-08 02:54:15 -0500

    Finally. Thanks jQuery

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3000;
+
+var connections = 0;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -15,15 +17,24 @@ app.get('/about', function(req, res){
 	res.send('<h1>About</h1>');
 });
 
+app.get('/test', function(req, res) {
+	res.send('<span>Hosts connected: ' + connections + '</span>');
+});
+
 io.on('connection', function(socket){
-  console.log('A user connected.');
+  connections = connections + 1;
+  console.log('A user connected. Users: ' + connections);
+
   socket.on('disconnect', function(){
-    console.log('A user disconnected.');
+    connections = connections - 1;
+    console.log('A user disconnected. Users: ' + connections);
   });
+
   socket.on('alert', function(msg){
   	console.log('An alert was emitted: ' + msg);
   	socket.broadcast.emit('alert', msg);
   });
+
 });
 
 http.listen(port, function(){
